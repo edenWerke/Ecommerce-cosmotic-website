158bb830c30476eedb12765f57386100179923ad
edenWerke <edenw6292@gmail.com>
2026-04-18 11:12:18 +0300

    add

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Test route
+// app.get("/", (req, res) => {
+//   res.send("Konjit Backend Running...");
+// });
+
+// // Connect DB
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
+
+// // Start server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Konjit Backend Running...");
-});
-
-// Connect DB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Start server
+app.get("/", (req, res) => {
+  res.send("Konjit API Running...");
+});
+
+// routes
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
